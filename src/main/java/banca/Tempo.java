ad1a7d2b87c6d747432017f104cf0068fcf03727
RakuJa <github@danielegiachetto.com>
2025-03-20 20:36:04 +0100

    Auto format code every push (remember to git pull) (#2)

--- src/main/java/banca/Tempo.java
+++ src/main/java/banca/Tempo.java
@@ -14,116 +14,122 @@ import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 public class Tempo extends JFrame {
-	private int week;
+  private int week;
 
-	public Tempo(String filePath, String pathGrafico) {
-		setTitle("DEUTCH BANK");
-		setSize(400, 300);
-		addWindowListener(new WindowAdapter() {
-			@SuppressWarnings("unused")
-			public void WindowClosing(WindowEvent e) {
-				FileTools.esciEntra(filePath, 0);
-				dispose();
-			}
-		});
-		setLocationRelativeTo(null);
+  public Tempo(String filePath, String pathGrafico) {
+    setTitle("DEUTCH BANK");
+    setSize(400, 300);
+    addWindowListener(
+        new WindowAdapter() {
+          @SuppressWarnings("unused")
+          public void WindowClosing(WindowEvent e) {
+            FileTools.esciEntra(filePath, 0);
+            dispose();
+          }
+        });
+    setLocationRelativeTo(null);
 
-		JPanel panel = new JPanel();
-		panel.setLayout(null);
-		add(panel);
+    JPanel panel = new JPanel();
+    panel.setLayout(null);
+    add(panel);
 
-		String ultimaRiga = FileTools.leggiUltimariga(filePath);
+    String ultimaRiga = FileTools.leggiUltimariga(filePath);
 
-		String[] dati = ultimaRiga.split(";");
-		double portafoglio = Double.parseDouble(dati[2]);
-		double contoCorrente = Double.parseDouble(dati[1]);
-		int settimana = Integer.parseInt(dati[0]);
+    String[] dati = ultimaRiga.split(";");
+    double portafoglio = Double.parseDouble(dati[2]);
+    double contoCorrente = Double.parseDouble(dati[1]);
+    int settimana = Integer.parseInt(dati[0]);
 
-		JLabel settimanaLabel = new JLabel("Sei alla " + settimana + " settimana");
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				int labelWidth = settimanaLabel.getPreferredSize().width;
+    JLabel settimanaLabel = new JLabel("Sei alla " + settimana + " settimana");
+    SwingUtilities.invokeLater(
+        new Runnable() {
+          @Override
+          public void run() {
+            int labelWidth = settimanaLabel.getPreferredSize().width;
 
-				int x = (400 - labelWidth) / 2;
+            int x = (400 - labelWidth) / 2;
 
-				int y = 60;
+            int y = 60;
 
-				settimanaLabel.setBounds(x, y, labelWidth, 25);
-			}
-		});
-		panel.add(settimanaLabel);
+            settimanaLabel.setBounds(x, y, labelWidth, 25);
+          }
+        });
+    panel.add(settimanaLabel);
 
-		JLabel dateLabel = new JLabel("Avanzamento:");
-		dateLabel.setBounds(50, 100, 100, 25);
-		panel.add(dateLabel);
+    JLabel dateLabel = new JLabel("Avanzamento:");
+    dateLabel.setBounds(50, 100, 100, 25);
+    panel.add(dateLabel);
 
-		JTextField dateField = new JTextField();
-		dateField.setBounds(150, 100, 200, 25);
-		panel.add(dateField);
+    JTextField dateField = new JTextField();
+    dateField.setBounds(150, 100, 200, 25);
+    panel.add(dateField);
 
-		JButton confirmButton = new JButton("Conferma");
-		confirmButton.setBounds(150, 150, 100, 25);
-		panel.add(confirmButton);
+    JButton confirmButton = new JButton("Conferma");
+    confirmButton.setBounds(150, 150, 100, 25);
+    panel.add(confirmButton);
 
-		confirmButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String mesiString = dateField.getText().trim();
-				action(filePath, pathGrafico, panel, mesiString, settimana, portafoglio, contoCorrente);
-			}
-		});
-	}
+    confirmButton.addActionListener(
+        new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            String mesiString = dateField.getText().trim();
+            action(filePath, pathGrafico, panel, mesiString, settimana, portafoglio, contoCorrente);
+          }
+        });
+  }
 
-	/**
-	 * Metodo che racchiude tutti i controlli collegati al botton "Conferma"
-	 *
-	 * @param filePath      -- Percorso del file interessato
-	 * @param pathGrafico   -- Percorso per il file dedicato al grafico
-	 * @param panel         -- JPanel al quale si collega la finestra per mandare
-	 *                      messaggi
-	 * @param mesiString    -- Variabile che contiene la stringa inserita
-	 *                      dall'utente nel campo dateField
-	 * @param settimana     -- Settimana attuale dell'operazione
-	 * @param portafoglio   -- Valore portafoglio attualmente
-	 * @param contoCorrente -- Valore conto corrente attualmente
-	 */
-	private void action(String filePath, String pathGrafico, JPanel panel, String mesiString, int settimana,
-			double portafoglio, double contoCorrente) {
-		week = 0;
-		boolean giusto = true;
+  /**
+   * Metodo che racchiude tutti i controlli collegati al botton "Conferma"
+   *
+   * @param filePath -- Percorso del file interessato
+   * @param pathGrafico -- Percorso per il file dedicato al grafico
+   * @param panel -- JPanel al quale si collega la finestra per mandare messaggi
+   * @param mesiString -- Variabile che contiene la stringa inserita dall'utente nel campo dateField
+   * @param settimana -- Settimana attuale dell'operazione
+   * @param portafoglio -- Valore portafoglio attualmente
+   * @param contoCorrente -- Valore conto corrente attualmente
+   */
+  private void action(
+      String filePath,
+      String pathGrafico,
+      JPanel panel,
+      String mesiString,
+      int settimana,
+      double portafoglio,
+      double contoCorrente) {
+    week = 0;
+    boolean giusto = true;
 
-		try {
-			week = Integer.parseInt(mesiString);
-		} catch (NumberFormatException e1) {
-			giusto = false;
-		}
+    try {
+      week = Integer.parseInt(mesiString);
+    } catch (NumberFormatException e1) {
+      giusto = false;
+    }
 
-		if (giusto && week <= 0) {
-			JOptionPane.showMessageDialog(panel, "Dato errato", "Errore", JOptionPane.ERROR_MESSAGE);
-		} else {
-			JOptionPane.showMessageDialog(panel, "Avanzamento svolto");
+    if (giusto && week <= 0) {
+      JOptionPane.showMessageDialog(panel, "Dato errato", "Errore", JOptionPane.ERROR_MESSAGE);
+    } else {
+      JOptionPane.showMessageDialog(panel, "Avanzamento svolto");
 
-			int tempo = settimana + week;
-			portafoglio += (100 * week);
+      int tempo = settimana + week;
+      portafoglio += (100 * week);
 
-			try {
-				FileTools.aggiungiNuovaRiga(filePath, portafoglio, contoCorrente, tempo, "Avanzamento");
-				FileTools.aggiungiFileGrafico(pathGrafico, portafoglio, contoCorrente, settimana);
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-			dispose();
-		}
-	}
+      try {
+        FileTools.aggiungiNuovaRiga(filePath, portafoglio, contoCorrente, tempo, "Avanzamento");
+        FileTools.aggiungiFileGrafico(pathGrafico, portafoglio, contoCorrente, settimana);
+      } catch (IOException e1) {
+        e1.printStackTrace();
+      }
+      dispose();
+    }
+  }
 
-	/**
-	 * Metodo per prendere le settimane attuali
-	 *
-	 * @return settimane attuali
-	 */
-	public int getWeek() {
-		return week;
-	}
-
+  /**
+   * Metodo per prendere le settimane attuali
+   *
+   * @return settimane attuali
+   */
+  public int getWeek() {
+    return week;
+  }
 }
--- src/test/java/banca/DepositaPrelevaTest.java
+++ src/test/java/banca/DepositaPrelevaTest.java
@@ -5,28 +5,27 @@ import org.junit.jupiter.api.Test;
 
 class DepositaPrelevaTest {
 
-	@Test
-	void test() {
-		// fai due test separati
-		String azione = "Deposita"; // "Deposita" o "Preleva"
-		String filePath = "C:\\Users\\Utente\\Documents\\AURORA\\BancaTpsit\\prova.csv";
-		DepositaPreleva prova = new DepositaPreleva(filePath, azione);
+  @Test
+  void test() {
+    // fai due test separati
+    String azione = "Deposita"; // "Deposita" o "Preleva"
+    String filePath = "C:\\Users\\Utente\\Documents\\AURORA\\BancaTpsit\\prova.csv";
+    DepositaPreleva prova = new DepositaPreleva(filePath, azione);
 
-		double conto = 120, portafoglio = 20, soldiPrelevareDepositare = 12;
-		double[] dati;
-		if (azione.equals("Deposita")) {
-			dati = prova.depositoPrelievo(null, true, portafoglio, conto, soldiPrelevareDepositare);
-		} else {
-			dati = prova.depositoPrelievo(null, true, conto, portafoglio, soldiPrelevareDepositare);
-		}
+    double conto = 120, portafoglio = 20, soldiPrelevareDepositare = 12;
+    double[] dati;
+    if (azione.equals("Deposita")) {
+      dati = prova.depositoPrelievo(null, true, portafoglio, conto, soldiPrelevareDepositare);
+    } else {
+      dati = prova.depositoPrelievo(null, true, conto, portafoglio, soldiPrelevareDepositare);
+    }
 
-		portafoglio = dati[0];
-		conto = dati[1];
+    portafoglio = dati[0];
+    conto = dati[1];
 
-		boolean risultato = !(portafoglio < 0) && !(conto < 0);
+    boolean risultato = !(portafoglio < 0) && !(conto < 0);
 
-        Assert.assertTrue(risultato);
-		prova.dispose();
-	}
-
+    Assert.assertTrue(risultato);
+    prova.dispose();
+  }
 }
